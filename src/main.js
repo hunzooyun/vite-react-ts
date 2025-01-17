36569cce2ddb8f0878bd06e7b5ccfdbd7d17e1ba
hunzooyun <hunzooyun@gmail.com>
2025-01-17 14:40:40 +0900

    react, react-dom 패키지 설치 및 초기 앱 구동

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import "./styles/main.css";
+import { StrictMode, createElement as h } from "react";
+import { createRoot } from "react-dom/client";
+
+const root = document.getElementById("root");
+
+if (root) {
+  createRoot(root).render(
+    h(StrictMode, null, h("h1", { className: "Headline", children: "안녕" }))
+  );
+}
