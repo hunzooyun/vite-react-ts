f511f6f708feec82b5a8327bfffbfb01c6838515
hunzooyun <hunzooyun@gmail.com>
2025-01-20 10:14:50 +0900

    degit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles/main.css";
-import App from "./app/app";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@/styles/main.css';
+import App from '@/app';
 
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 
 if (root) {
   createRoot(root).render(
